8611c006f4e6264322d72deae17cb495da2b962b
ctrlv <carlvalero14@gmail.com>
2020-11-27 14:20:11 +0800

    python files added

---
+++ python/edx.py
@@ -0,0 +1,33 @@
+#name = input("Input your Name:")
+#print(f"Hello {name}!")
+
+#condition
+
+#PYTHON DATA STRUCTURE
+#sequence.py
+#array
+#tupples
+
+#list.py list append  and  sort  method
+ 
+ #SET.PY add and remove and len
+
+ #looping for in
+
+ #dictionary 
+
+ #fucntion.y sqaure 
+
+ #square.py import function
+
+ #class OOP
+
+class Point():
+  def __init__ (self,x,y):
+    self.xaxis = x
+    self.yaxis = y
+
+p = Point(2,4)
+print(p.xaxis, p.yaxis)
+
+  
